37313a925d7237d65bbd7f0a2573305887539246
Vlad <ilchucvlad@gmail.com>
2024-11-21 16:05:50 +0200

    7
    1

--- src/PortfolioPage.js
+++ src/PortfolioPage.js
@@ -5,33 +5,33 @@ export default {
       projects: [
         {
           id: 1,
-          title: "Проект 1",
-          description: "Опис проекту 1.",
+          title: "Проєкт 1",
+          description: "Опис Проєкту 1.",
           link: "https://project1.com",
           backgroundImage: "url('https://example.com/site_5.png')", 
         },
         {
           id: 2,
-          title: "Проект 2",
-          description: "Опис проекту 2.",
+          title: "Проєкт 2",
+          description: "Опис Проєкту 2.",
           link: "https://project2.com",
         },
         {
           id: 3,
-          title: "Проект 3",
+          title: "Проєкт 3",
           description: "Опис проекту 3.",
           link: "https://project3.com",
         },
         {
           id: 4,
-          title: "Проект 4",
-          description: "Опис проекту 4.",
+          title: "Проєкт 4",
+          description: "Опис Проєкту 4.",
           link: "https://project4.com",
         },
         {
           id: 5,
-          title: "Проект 5",
-          description: "Опис проекту 5.",
+          title: "Проєкт 5",
+          description: "Опис Проєкту 5.",
           link: "https://project5.com",
         },
         {
