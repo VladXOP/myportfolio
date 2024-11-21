458c2a68aeff448379e38bb5a2fab859564cede4
Vlad <ilchucvlad@gmail.com>
2024-11-21 15:58:49 +0200

    4
    1

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/myportfolio/'
-    : '/'
+    : '/',
+  devServer: {
+    host: '192.168.0.101', // Доступ з локальної мережі
+    port: 8082, // Порт, який ви використовуєте
+    allowedHosts: 'all', // Дозволяє доступ з усіх хостів
+  }
 });
